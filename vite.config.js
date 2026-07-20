d7330a3f7a58371753ba1c6ce88fe1762a1dc849
nikeshwar pandey <nikeshwarpandey@gmail.com>
2026-07-20 21:01:19 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/nikesh_portfolio/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+}));
